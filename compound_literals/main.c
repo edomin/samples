018f93023f1a3b184bbf7768e266d3f6d99c1548
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2021-05-06 22:53:06 +0300

    Move test struct to global space

--- compound_literals/main.c
+++ compound_literals/main.c
@@ -5,11 +5,12 @@ typedef struct {
     int  *numeric;
 } test_t;
 
+test_t test_struct = {
+    "test",
+    (int[]){0, 1, 2, 3},
+};
+
 int main(int argc, char **argv) {
-    test_t test_struct = {
-        "test",
-        (int[]){0, 1, 2, 3},
-    };
 
     printf("sizeof(test_t): %zu\n", sizeof(test_t));
     printf("test_struct->text: %s\n", test_struct.text);
